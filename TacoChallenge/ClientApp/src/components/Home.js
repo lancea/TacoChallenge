6cd40ac0f609f1b206065af8062b5ecf250e347a
Lance <lance@whereismytransport.com>
2019-10-13 23:01:53 +0200

    do=down

--- TacoChallenge/ClientApp/src/components/Home.js
+++ TacoChallenge/ClientApp/src/components/Home.js
@@ -7,7 +7,7 @@ export class Home extends Component {
         return (
             <div>
                 <p>The endpoint for restaurants is at <a href="/api/restaurants">/api/restaurants</a></p>
-                <p>I went do the wrong rabbit hole by trying to use EF with the sample dataset :/</p>                
+                <p>I went down the wrong rabbit hole by trying to use EF with the sample dataset :/</p>                
             </div>
         );
     }
